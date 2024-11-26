fea8cba056bc10e70bfe2f70089e935bdb0885d6
Kévin <kevindubuc62@gmail.com>
2024-11-26 23:28:23 +0100

    amélioration de l'ui/ux

---
+++ src/scripts/sidebar.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    new Sidebar('#sidebar');
+});
+
+class Sidebar {
+    constructor(querySelector) {
+      this.elem = document.querySelector(querySelector);  
+      if (this.elem !== null) {
+        this.expandActiveGroup();
+      }
+    }
+
+    getActiveLink() {
+      let activeLink = this.elem.querySelector(`a.active-link`);
+      return activeLink;
+    }
+  
+    expandActiveGroup() {
+      const activeLink = this.getActiveLink();
+  
+      if (activeLink) {
+        let group = activeLink.closest('.sidebar-group');
+        while (group) {
+          this.toggleGroup(group, false);
+          group = group.parentElement.closest('.sidebar-group');
+        }
+      }
+    }
+
+    toggleGroup(group, hide) {
+      let items = group.querySelector('.nav.collapse');
+      console.log(items);
+      items.classList.toggle('hide', hide);
+      items.classList.toggle('show', !hide);
+    }
+  }
