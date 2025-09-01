a4197aa8816f9b03f87228212c43d7390283f6ef
Kévin DUBUC <kevindubuc62@gmail.com>
2025-09-01 15:09:30 +0200

    minfy html

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const sass = require("eleventy-sass");
 const yaml = require("js-yaml");
+const htmlmin = require("html-minifier-terser");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(sass);
@@ -10,6 +11,22 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/scripts");
   eleventyConfig.addPassthroughCopy("src/error.html");
+
+  // Minify HTML
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+
+      return minified;
+    }
+
+    // If not an HTML output, return content as-is
+    return content;
+  });
   
   return {
     templateFormats: ['html', 'liquid', 'md', 'njk'],
