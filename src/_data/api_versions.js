c72c591d0b91a96a971ee4107294b3636a4f4fb3
Kévin DUBUC <kevindubuc62@gmail.com>
2025-10-24 17:30:43 +0200

    Refactor sidebar layout and navigation styles
    Reworked sidebar.liquid to use a grid-based layout, removed version selection and related data files, and updated navigation to dynamically show guides or API links. SCSS files were updated to support the new layout, improve navbar and sidebar styling, and remove redundant main background styles.

--- src/_data/api_versions.js
+++
@@ -1,8 +0,0 @@
-const EleventyFetch = require("@11ty/eleventy-fetch");
-
-module.exports = async function () {
-	return EleventyFetch("https://api.github.com/repos/metarisc/openapi/branches", {
-		duration: "1200s",
-		type: "json",
-	});
-};
